8547d213b4edcef947c99efebc4bfb8534566ec2
Marcel Strahl <info@marcel-strahl.de>
2024-05-20 10:18:16 +0200

    Update paths for storage calls in ServiceProviderTest
    This commit updates the storage paths used for 'exists' and 'makeDirectory' methods in ServiceProviderTest to be absolute rather than relative. Also, it sets the 'useStoragePath' for the application to the updated path, ensuring consistency between tests and application storage paths.

--- tests/Feature/ServiceProviderTest.php
+++ tests/Feature/ServiceProviderTest.php
@@ -40,10 +40,11 @@ final class ServiceProviderTest extends TestCase
             ->method('get')
             ->willReturnOnConsecutiveCalls([], true, 'json', false, true, []);
 
-        Storage::shouldReceive('exists')->once()->with('/storage/framework/cache/data')->andReturn(false);
-        Storage::shouldReceive('makeDirectory')->with('/storage/framework/cache/data')->andReturn(true);
+        Storage::shouldReceive('exists')->once()->with(__DIR__.'/data/storage/framework/cache/data')->andReturn(false);
+        Storage::shouldReceive('makeDirectory')->with(__DIR__.'/data/storage/framework/cache/data')->andReturn(true);
 
         $this->application = new Application();
+        $this->application->useStoragePath(__DIR__.'/data');
 
         $this->application->bind('config', fn () => $configRepository);
 
